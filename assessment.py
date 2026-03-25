3327d497dfd08811c655ae77840b09a2e6b4e99f
Kian Dela Cruz <22kdelacruz@wakatipu.school.nz>
2026-03-25 13:49:17 +1300

    fixed sum stuff

--- assessment.py
+++ assessment.py
@@ -1,36 +1,26 @@
+student_info = {'001' : ["Kian Dela Cruz", 17, 11, 50, 25, 25],
+            '002' : ["Nhico Bigcas", 16, 11, 25, 50, 25],
+            '003' : ["Justin Pelayo", 16, 12, 25, 50, 50],
+            '004' : ["Mishael Masiglat", 18, 12, 50, 50, 50]}
+
 def display_menu():
     print("What would you like to do?")
     print("1. View student's info.")
-    print("2. View NCEA.")
+    print("2. View credits.")
     print("3. View endorsements.")
-    print("4. View a year level..")
+    print("4. View a year level.")
     print("5. Add credits.")
     print("6. Add students.")
     print("7. View summary.")
-
-student_info = {'001' : ["Kian Dela Cruz", 17, 12, 50, 25, 25],
-            '002' : ["Nhico Bigcas", 16, 12, 25, 50, 25],
-            '003' : ["Justin Pelayo", 16, 12, 25, 50, 50],
-            '004' : ["Mishael Masiglat", 18, 11, 50, 50, 50]}
+    print("8. End program")
 
 def view_data():
     for stu in student_info:
         print(f"ID: {(stu)} Name: {student_info[stu][0]} Age: {student_info[stu][1]} Year: {student_info[stu][2]}")
 
-def view_ncea():
+def view_credits():
     for stu in student_info:
         print(f"ID: {(stu)} Achieved: {student_info[stu][3]} Merit: {student_info[stu][4]} Excellence: {student_info[stu][5]}")
-
-def view_summary():
-    for stu in student_info:
-        print(f"ID: {(stu)} Name: {student_info[stu][0]} Age: {student_info[stu][1]} Year: {student_info[stu][2]} ")
-        if student_info[stu][3] >= 50:
-            print("Endorsement: Achieved")
-        if student_info[stu][4] >=50:
-            print("Endorsement: Merit")
-        if student_info[stu][5] >= 50:
-            print("Endorsement: Excellence")
-        print()
 
 def view_endorsements():
     for stu in student_info:
@@ -43,7 +33,55 @@ def view_endorsements():
             print("Endorsement: Excellence")
         print("-----")
 
-def get_int()
+def get_int(prompt):
+    while True:
+        try:
+            num = int(input(prompt))
+            return num
+        except ValueError:
+            print("That is not a valid integer, try again.")
+
+def student_year(student_info):
+    year = get_int("From what year do you want to see?: ")
+    print()
+    print("-----")
+    for stu in student_info:
+        if student_info[stu][2] == year:
+            print(f"Name: {student_info[stu][0]} Age: {student_info[stu][1]} Year: {student_info[stu][2]}")
+    print("-----")
+
+def add_cred(student_info, get_int):
+    while True:
+        print("-----")
+        identification = input("From what ID do you want to add to: ")
+        cred = get_int("How many credits do you want to add?: ")
+        level = input("What level: ").lower()
+        if level == "achieved":
+            student_info[identification][3] += cred
+        elif level == "merit":
+            student_info[identification][4] += cred
+        elif level == "excellence":
+            student_info[identification][5] += cred
+        else:
+            print("Invalid level, please input Achieved, Merit, or Excellence")
+            print("Try again")
+            print()
+            continue
+        print("All done!")
+        print("-----")
+        print()
+        break
+
+def view_summary():
+    for stu in student_info:
+        print(f"ID: {(stu)} Name: {student_info[stu][0]} Age: {student_info[stu][1]} Year: {student_info[stu][2]} ")
+        if student_info[stu][3] >= 50:
+            print("Endorsement: Achieved")
+        if student_info[stu][4] >=50:
+            print("Endorsement: Merit")
+        if student_info[stu][5] >= 50:
+            print("Endorsement: Excellence")
+        print()
 
 while True:
     print()
@@ -57,40 +95,25 @@ while True:
         print("-----")
     elif choice == "2":
         print("-----")
-        view_ncea()
+        view_credits()
         print("-----")
     elif choice == "3":
         print("-----")
         view_endorsements()
         print()
     elif choice == "4":
-        year = input("From what year do you want to see?: ")
-        print(f"Year {year}")
-        for stu in student_info:
-            if student_info[stu][2] == year:
-                pass
+        student_year(student_info)
     elif choice == "5":
-        while True:
-            print("-----")
-            identification = input("From what ID do you want to add to: ")
-
-            level = input("What level: ").lower()
-            cred = int(input("How many credits?: "))
-            
-            if level == "achieved":
-                student_info[identification][3] += cred
-            elif level == "merit":
-                student_info[identification][4] += cred
-            elif level == "excellence":
-                student_info[identification][5] += cred
-            else:
-                print("Try again")
-                continue
-            print("All done!")
-            print("-----")
-            print()
-            break
+        add_cred(student_info, get_int)
+    elif choice == "6":
+        pass
     elif choice == "7":
         print("---")
         view_summary()
         print("---")
+    elif choice == "8":
+        print("Thanks see you next time")
+        break 
+    else: 
+        print("Invalid input, try again.")
+        continue
